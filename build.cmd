fcefce4e4a32aad25753a24bd5936eab1b7e220c
Martins Mozeiko <martins.mozeiko@gmail.com>
2017-10-31 15:40:57 -0700

    support building from different drive

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat" -arch=amd64 -host_arch=amd64
-cd "%~dp0"
+cd /d "%~dp0"
 
 set CL=/nologo /errorReport:none /Gm- /GF /GS- /MT /W4 /WX /wd4324 /D_CRT_SECURE_NO_DEPRECATE
 set LINK=/errorReport:none /INCREMENTAL:NO
