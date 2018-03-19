d9225cebddc86c8a5dd127b37a2038743a5b84f7
Lucas <Gnarmagon@users.noreply.github.com>
2018-03-19 20:27:11 +0100

    fixed coding style

--- pdb.h
+++ pdb.h
@@ -1,5 +1,4 @@
 #include <stdint.h>
 #include <stddef.h>
-int check_dir(const char *directory_path);
-size_t writeFile(const char *path, const uint8_t *buf, const uint32_t length);
-uint32_t pkgdbGenerate(uint8_t *buffer, uint32_t length, char *title, char *titleid, char *contentid, const char *pkg_name, char *pkg_url, uint32_t install_id);
+size_t writeFile(const char* path, const uint8_t* buf, const uint32_t length);
+uint32_t pkgdbGenerate(uint8_t* buffer, uint32_t length, char* title, char* titleid, char* contentid, const char* pkg_name, char* pkg_url, uint32_t install_id);
