54951924517e6fe1c59c4f81cd80c00ed35c7c8e
Lucas <Gnarmagon@users.noreply.github.com>
2018-03-19 16:09:16 +0100

    Add files via upload

--- build.cmd
+++ build.cmd
@@ -10,4 +10,5 @@ set CL=%CL% /Ox
 rem set CL=%CL% /Od /Zi
 rem set LINK=%LINK% /DEBUG
 
-cl.exe pkg2zip*.c miniz_tdef.c puff.c /Fepkg2zip.exe
+cl.exe pkg2zip*.c miniz_tdef.c puff.c pdb.c /Fepkg2zip.exe
+pause
